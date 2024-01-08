bc17dc0ba6fab0e2c6d17ffb635007b0186b2e6d
HectorB <elmillo1509@gmail.com>
2024-01-08 02:04:21 -0300

    feat/front: se agrega la persistencia del usuario logeado, falta logout

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
-import ModalAdd from "../modals/ModalAdd";
 import ChecksTable from "../table/ChecksTable";
 import Login from "../login/Login";
 
 const Home = () => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const user = localStorage.getItem("token");
+    const [isAuthenticated, setIsAuthenticated] = useState(user? true: false);
+
 
     const handleLoginSuccess = () => {
-        setIsAuthenticated(true);
+        if(user){
+            setIsAuthenticated(true);
+        }
     };
 
     return (
         <div className="container pt-3">
             {!isAuthenticated && <Login onLoginSuccess={handleLoginSuccess} />}
-            {isAuthenticated && <ModalAdd />}
             {isAuthenticated && <ChecksTable />}
         </div>
     );
